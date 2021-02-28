556f087605fa52eb98b4848a43e95bfab3c9a5eb
Ward Nijs <ward_nijs@hotmail.com>
2021-02-28 08:39:34 +0100

    Add project files.

---
+++ MotoGP/Controllers/ShopController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoGP.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult OrderTicket()
+        {
+            ViewData["BannerNr"] = 3;
+
+            return View();
+        }
+
+        public IActionResult ConfirmOrder()
+        {
+            ViewData["BannerNr"] = 3;
+
+            return View();
+        }
+
+    }
+}
