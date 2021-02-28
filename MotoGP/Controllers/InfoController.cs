b1c01184c48938b9f279f2a17a977c9ffe2700fd
Ward Nijs <ward_nijs@hotmail.com>
2021-02-28 08:53:39 +0100

    Race klasse & infocontroller

---
+++ MotoGP/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoGP.Controllers
+{
+    public class InfoController : Controller
+    {
+       public IActionResult ListRaces()
+        {
+            ViewData["BannerNr"] = 0;
+            ViewData["Title"] = "Races";
+            return View();
+        }
+    }
+}
---
+++ MotoGP/Models/Race.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoGP.Models
+{
+    public class Race
+    {
+        public int RaceID { get; set; }
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
